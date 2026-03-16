ee0569c3690852b892a0178e555976257276dbb5
Q1-byte <gyuwon8440@gmail.com>
2026-03-16 16:50:48 +0900

    feat: GET /api/canvas/{id} 세션 조회 API 구현

--- src/main/java/com/egag/canvas/CanvasService.java
+++ src/main/java/com/egag/canvas/CanvasService.java
@@ -2,7 +2,9 @@ package com.egag.canvas;
 
 import com.egag.common.domain.ArtworkRepository;
 import com.egag.common.domain.UserRepository;
+import com.egag.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +17,11 @@ public class CanvasService {
     private final ThemeAgent themeAgent;
     private final ColorAgent colorAgent;
 
-    // TODO: startSession, processStroke, suggestTopics, confirmTopic, colorize, save, complete, getSession
+    public CanvasSessionResponse getSession(String id) {
+        return artworkRepository.findById(id)
+                .map(CanvasSessionResponse::from)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "CANVAS_NOT_FOUND", "캔버스 세션을 찾을 수 없습니다"));
+    }
+
+    // TODO: startSession, processStroke, suggestTopics, confirmTopic, colorize, save, complete
 }
