0802dcf465387567ab57676df117d2d893e74d52
규원 <qone@gyuwon-ui-MacBookPro.local>
2026-09-08 14:25:38 +0900

    fix: CORS에 qones.site 추가

--- src/main/java/com/egag/common/config/CorsConfig.java
+++ src/main/java/com/egag/common/config/CorsConfig.java
@@ -18,6 +18,7 @@ public class CorsConfig {
             "http://localhost:5173",
             "http://43.203.93.46",
             "https://egag.qones.site",
+            "https://qones.site",
             "http://egag-frontend-390445053104-ap-northeast-2-an.s3-website.ap-northeast-2.amazonaws.com"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
